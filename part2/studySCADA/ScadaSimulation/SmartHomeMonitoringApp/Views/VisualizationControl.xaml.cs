8b7dc1e81c9c9978a946f93d02680d16686e07ff
mimi9155 <hanbv@naver.com>
2023-05-12 11:40:10 +0900

    -

--- part2/studySCADA/ScadaSimulation/SmartHomeMonitoringApp/Views/VisualizationControl.xaml.cs
+++ part2/studySCADA/ScadaSimulation/SmartHomeMonitoringApp/Views/VisualizationControl.xaml.cs
@@ -1,14 +1,24 @@
-﻿using MySql.Data.MySqlClient;
-using OxyPlot;
-using OxyPlot.Legends;
+﻿using OxyPlot;
 using OxyPlot.Series;
+using MySql.Data.MySqlClient;
 using SmartHomeMonitoringApp.Logics;
 using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using OxyPlot.Legends;
 
 namespace SmartHomeMonitoringApp.Views
 {
@@ -125,8 +135,8 @@ namespace SmartHomeMonitoringApp.Views
                 await Commons.ShowCustomMessageAsync("DB검색", $"DB검색 오류 {ex.Message}");
             }
 
-            // DB에서 가져온 데이터 차트에 뿌리도록 처리
-            var tmp = new PlotModel { Title = $"{CboRoomName.SelectedValue}Room" };
+            // Create the plot model // 선택한 방의 이름이 타이틀로 나오도록
+            var tmp = new PlotModel { Title = $"{CboRoomName.SelectedValue} ROOM", DefaultFont = "NanumGothic" };
             var legend = new Legend
             {
                 LegendBorder = OxyColors.DarkGray,
@@ -134,38 +144,35 @@ namespace SmartHomeMonitoringApp.Views
                 LegendPosition = LegendPosition.TopRight,
                 LegendPlacement = LegendPlacement.Outside,
             };
-            tmp.Legends.Add(legend);
+            tmp.Legends.Add(legend); // 범례추가
 
             // Create two line series (markers are hidden by default)
             var tempSeries = new LineSeries
             {
                 Title = "Temperature(℃)",
                 MarkerType = MarkerType.Circle,
-                Color = OxyColors.DarkOrange,
+                Color = OxyColors.DarkOrange, // 라인색상 온도는 주황색
             };
-
-
             var humidSeries = new LineSeries
             {
                 Title = "Humidity(%)",
                 MarkerType = MarkerType.Square,
-                Color = OxyColors.Aqua,
+                Color = OxyColors.Aqua, // 습도는 물색
             };
 
+            // DB에서 가져온 데이터 차트에 뿌리도록 처리
             if (ds.Tables[0].Rows.Count > 0)
             {
+                TotalDataCount = ds.Tables[0].Rows.Count;
 
                 var count = 0;
-
                 foreach (DataRow row in ds.Tables[0].Rows)
                 {
                     tempSeries.Points.Add(new DataPoint(count++, Convert.ToDouble(row["Temp"])));
                     humidSeries.Points.Add(new DataPoint(count++, Convert.ToDouble(row["Humid"])));
                 }
-
-
             }
-            // Add the series to the plot model
+
             tmp.Series.Add(tempSeries);
             tmp.Series.Add(humidSeries);
 
